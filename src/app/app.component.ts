f00204941760bd0a05b8b3817ee343f505eb4517
tarun.utkarsh <tarun.utkarsh@globallogic.com>
2023-06-28 22:03:46 +0530

    Completed app

---
+++ src/app/app.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from './services/weather.service';
+import { WeatherData } from './models/weather.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  title = 'WeatherApp';
+
+  constructor(private weatherService: WeatherService){
+
+  }
+
+  cityName: string="Kanpur";
+  weatherData?: WeatherData;
+
+  ngOnInit(): void {
+    this.getWeatherData(this.cityName);
+    // this.cityName='';
+  }
+
+  onSubmit(){
+    this.getWeatherData(this.cityName);
+    // this.cityName='';
+  }
+
+  private getWeatherData(cityName : string){
+    this.weatherService.getWeatherData(cityName)
+      .subscribe({
+        next:(response) =>{
+          this.weatherData = response;
+          console.log(response)
+            
+        },
+      });
+  }
+}
---
+++ src/environment/environment.ts
@@ -0,0 +1,15 @@
+export const environment = {
+    production : false,
+    // weatherApiBaseUrl: 'https://openweather43.p.rapidapi.com/weather',
+    // XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+    // XRapidAPIKeyHeaderValue: '6d92634263msha742af76471afbcp1d1334jsn00ad904abe46',
+    // XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+    // XRapidAPIHostHeaderValue: 'openweather43.p.rapidapi.com'
+
+    // ninja weather api 
+    weatherApiBaseUrl: 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather',
+    XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+    XRapidAPIKeyHeaderValue: '6d92634263msha742af76471afbcp1d1334jsn00ad904abe46',
+    XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+    XRapidAPIHostHeaderValue: 'weather-by-api-ninjas.p.rapidapi.com'
+}
